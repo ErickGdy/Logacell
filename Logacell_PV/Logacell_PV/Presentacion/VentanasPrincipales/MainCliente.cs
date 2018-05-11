0568b62423ea06f65a8f8633207806f5bb16cc8c
Unknown <erick_andres27@hotmail.com>
2018-05-11 13:07:31 -0600

    Clientes frecuentes
    Campo de clientes "Frecuente" eliminado

--- Logacell_PV/Logacell_PV/Presentacion/VentanasPrincipales/MainCliente.cs
+++ Logacell_PV/Logacell_PV/Presentacion/VentanasPrincipales/MainCliente.cs
@@ -23,13 +23,10 @@ namespace Logacell
         public MainCliente()
         {
             InitializeComponent();
-            cmbNumByPag.SelectedIndex = 0;
-            cmbNumByPageF.SelectedIndex = 0;
             control = ControlLogacell.getInstance();
             try
             {
                 actualizarTablaClientes(control.obtenerClientesTable());
-                actualizarTablaClientesFrecuente(control.obtenerClientesFrecuentesTable());
             }
             catch (Exception e)
             {
@@ -47,19 +44,6 @@ namespace Logacell
         }
 
 
-        private void btnAgregarClienteF_Click(object sender, EventArgs e)
-        {
-            FormCliente form = new FormCliente(null);
-            form.FormClosed += new FormClosedEventHandler(form_ClosedFrecuente);
-            form.Show();
-        }
-        private void form_ClosedFrecuente(object sender, FormClosedEventArgs e)
-        {
-            //aqui actualizas o recargas la info del Form1
-            actualizarTablaClientes(control.obtenerClientesTable());
-            actualizarTablaClientesFrecuente(control.obtenerClientesFrecuentesTable());
-        }
-
         private void btnAgregarCliente_Click(object sender, EventArgs e)
         {
             FormCliente form = new FormCliente(null);
@@ -70,7 +54,6 @@ namespace Logacell
         {
             //aqui actualizas o recargas la info del Form1
             actualizarTablaClientes(control.obtenerClientesTable());
-            actualizarTablaClientesFrecuente(control.obtenerClientesFrecuentesTable());
         }
 
         public void actualizarTablaClientes(MySqlDataAdapter data)
@@ -89,34 +72,10 @@ namespace Logacell
                 dataGridView1.Columns[1].Width = 180;
                 dataGridView1.Columns[2].Width = 80;
                 dataGridView1.Columns[3].Width = 120;
-                dataGridView1.Columns[4].Width = 80;
-                dataGridView1.Columns[5].Width = 120;
+                dataGridView1.Columns[4].Width = 200;
+
             }
             catch(Exception e)
-            {
-                MessageBox.Show(e.Message);
-            }
-        }
-        public void actualizarTablaClientesFrecuente(MySqlDataAdapter data)
-        {
-            try
-            {
-                DataTable dtDatos = new DataTable();
-                // Con la información del adaptador se rellena el DataTable
-                data.Fill(dtDatos);
-
-                // Se asigna el DataTable como origen de datos del DataGridView
-                dataGridView2.DataSource = dtDatos;
-                // actualiza el valor de la etiqueta donde se muestra el total de productos
-                lblTotalFrecuentes.Text = dataGridView2.RowCount.ToString();
-                dataGridView2.Columns[0].Width = 180;
-                dataGridView2.Columns[1].Width = 180;
-                dataGridView2.Columns[2].Width = 80;
-                dataGridView2.Columns[3].Width = 120;
-                dataGridView2.Columns[4].Width = 80;
-                dataGridView2.Columns[5].Width = 120;
-            }
-            catch (Exception e)
             {
                 MessageBox.Show(e.Message);
             }
@@ -126,17 +85,6 @@ namespace Logacell
             try
             {
                 actualizarTablaClientes(control.obtenerClientesTable(txtBuscarCliente.Text));
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-        private void txtBuscarFrecuente_KeyUp(object sender, KeyEventArgs e)
-        {
-            try
-            {
-                actualizarTablaClientesFrecuente(control.obtenerClientesFrecuentesTable(txtBuscarFrecuente.Text));
             }
             catch (Exception ex)
             {
@@ -177,7 +125,6 @@ namespace Logacell
                     {
                         MessageBox.Show("Cliente Eliminado");
                         actualizarTablaClientes(control.obtenerClientesTable());
-                        actualizarTablaClientesFrecuente(control.obtenerClientesFrecuentesTable());
                     }
                     else MessageBox.Show("Error al eliminar cliente");
                 }
@@ -188,50 +135,9 @@ namespace Logacell
             }
         }
 
-        private void consultarFrecuenteToolStripMenuItem_Click(object sender, EventArgs e)
+        private void MainCliente_SizeChanged(object sender, EventArgs e)
         {
-            String telefono = dataGridView2.CurrentRow.Cells[2].Value.ToString();
-            MessageBox.Show("aqui se mostraran los datos relevantes del cliente: " + telefono);
-        }
-
-        private void modificarFrecuenteToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            String telefono = dataGridView2.CurrentRow.Cells[2].Value.ToString();
-            try
-            {
-                Cliente cliente = control.consultarCliente(telefono);
-                FormCliente fc = new FormCliente(cliente);
-                fc.FormClosed += new FormClosedEventHandler(form_ClosedClientes);
-                fc.Show();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void eliminarFrecuenteToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                String telefono = dataGridView2.CurrentRow.Cells[2].Value.ToString();
-                DialogResult dialogResult = MessageBox.Show("¿Desea eliminar el Cliente?", "Eliminar Cliente", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
-                if (dialogResult == DialogResult.OK)
-                {
-                    if (control.eliminarCliente(telefono))
-                    {
-                        MessageBox.Show("Cliente Eliminado");
-                        actualizarTablaClientes(control.obtenerClientesTable());
-                        actualizarTablaClientesFrecuente(control.obtenerClientesFrecuentesTable());
-                    }
-                    else
-                        MessageBox.Show("Error al eliminar cliente");
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            pnMenus.Height = this.Height;
         }
     }
 }
